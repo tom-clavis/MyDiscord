af8cdbc9d2923a25b8e79a8d486ef0ec2c906a09
romain.abdouh <romain.abdouh@laplateforme.io>
2024-03-05 18:56:44 +0100

    liaison une fois user connecté, affichage page liste channel

--- src/login_menu.py
+++ src/login_menu.py
@@ -13,36 +13,27 @@ class LoginMenu():
         self.master = master
         self.master.title("MyDiscord")
         self.user_session = None 
-    
+        
         custom_font = ("Arial Black", 11)
-
         self.master.config(bg="slateblue4")
-
         self.label_usermail = tk.Label(master, text="Adresse mail", font=custom_font)
         self.label_password = tk.Label(master, text="Mot de passe", font=custom_font)
-
         self.entry_usermail = tk.Entry(master, font=custom_font)
         self.entry_password = tk.Entry(master, show="*", font=custom_font)
-
         self.button_login = tk.Button(master, text="Connexion", command=self.login, font=custom_font)
         self.button_register = tk.Button(master, text="Inscription", command=self.open_register_menu, font=custom_font)
-
         self.label_usermail.pack(pady=10)
         self.entry_usermail.pack(pady=10)
         self.label_password.pack(pady=10)
         self.entry_password.pack(pady=10)
         self.button_login.pack(pady=10)
         self.button_register.pack(pady=10)
-
         self.master.geometry("600x450")
-
         self.logo_image = tk.PhotoImage(file="Images/logo3.png")
         self.logo_label = tk.Label(self.master, image=self.logo_image, bg="slateblue4")
         self.logo_label.pack(side=tk.BOTTOM, pady=10)
-
         self.user_manager = UserManager(host="localhost", user="root", password=mdp, database="MyDiscord")
         
-
     def login(self):
         email = self.entry_usermail.get()
         password = self.entry_password.get()
